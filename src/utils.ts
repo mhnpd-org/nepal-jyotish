90e4081b4c6c941eb262907f3656f774e62598b7
mhnpd <mhnpd@outlook.com>
2025-09-25 14:04:55 +0200

    refactor: remove unused getChartHouse utility function

--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-export function getChartHouse(originalHouse: number, lagnaDegrees: number) {
-  const lagnaHouse = Math.floor(lagnaDegrees / 30) + 1; // 1-12
-  // Formula to map original house to chart house
-  return ((originalHouse - lagnaHouse + 12) % 12) + 1;
-}
