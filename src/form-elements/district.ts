0569297778f24e3ada5eea96cb70c854b21f2384
mhnpd <mhnpd@outlook.com>
2025-10-01 10:51:58 +0200

    feat: add district data for Nepal with English and Nepali names, zones, and coordinates

---
+++ src/form-elements/district.ts
@@ -0,0 +1,541 @@
+export const districtOfNepal = [
+  {
+    district_en: "Taplejung",
+    district_np: "ताप्लेजुङ",
+    zone: "Mechi",
+    lat: 27.35,
+    long: 87.75
+  },
+  {
+    district_en: "Panchthar",
+    district_np: "पाँचथर",
+    zone: "Mechi",
+    lat: 27.15,
+    long: 87.8
+  },
+  {
+    district_en: "Ilam",
+    district_np: "इलाम",
+    zone: "Mechi",
+    lat: 26.9,
+    long: 88.0
+  },
+  {
+    district_en: "Jhapa",
+    district_np: "झापा",
+    zone: "Mechi",
+    lat: 26.6,
+    long: 87.95
+  },
+  {
+    district_en: "Sankhuwasabha",
+    district_np: "सङ्खुवासभा",
+    zone: "Koshi",
+    lat: 27.5,
+    long: 87.35
+  },
+  {
+    district_en: "Terhathum",
+    district_np: "तेह्रथुम",
+    zone: "Koshi",
+    lat: 27.15,
+    long: 87.58
+  },
+  {
+    district_en: "Bhojpur",
+    district_np: "भोजपुर",
+    zone: "Koshi",
+    lat: 27.15,
+    long: 87.0
+  },
+  {
+    district_en: "Dhankuta",
+    district_np: "धनकुटा",
+    zone: "Koshi",
+    lat: 27.0,
+    long: 87.35
+  },
+  {
+    district_en: "Morang",
+    district_np: "मोरङ",
+    zone: "Koshi",
+    lat: 26.6,
+    long: 87.45
+  },
+  {
+    district_en: "Sunsari",
+    district_np: "सुनसरी",
+    zone: "Koshi",
+    lat: 26.65,
+    long: 87.15
+  },
+  {
+    district_en: "Solukhumbu",
+    district_np: "सोलुखुम्बु",
+    zone: "Sagarmatha",
+    lat: 27.7,
+    long: 86.6
+  },
+  {
+    district_en: "Khotang",
+    district_np: "खोटाङ",
+    zone: "Sagarmatha",
+    lat: 27.15,
+    long: 86.85
+  },
+  {
+    district_en: "Okhaldhunga",
+    district_np: "ओखलढुङ्गा",
+    zone: "Sagarmatha",
+    lat: 27.3,
+    long: 86.4
+  },
+  {
+    district_en: "Udayapur",
+    district_np: "उदयपुर",
+    zone: "Sagarmatha",
+    lat: 26.9,
+    long: 86.75
+  },
+  {
+    district_en: "Saptari",
+    district_np: "सप्तरी",
+    zone: "Sagarmatha",
+    lat: 26.55,
+    long: 86.85
+  },
+  {
+    district_en: "Siraha",
+    district_np: "सिराहा",
+    zone: "Sagarmatha",
+    lat: 26.65,
+    long: 86.25
+  },
+  {
+    district_en: "Dolakha",
+    district_np: "दोलखा",
+    zone: "Janakpur",
+    lat: 27.75,
+    long: 86.15
+  },
+  {
+    district_en: "Ramechhap",
+    district_np: "रामेछाप",
+    zone: "Janakpur",
+    lat: 27.35,
+    long: 86.2
+  },
+  {
+    district_en: "Sindhuli",
+    district_np: "सिन्धुली",
+    zone: "Janakpur",
+    lat: 27.25,
+    long: 85.9
+  },
+  {
+    district_en: "Dhanusha",
+    district_np: "धनुषा",
+    zone: "Janakpur",
+    lat: 26.85,
+    long: 85.95
+  },
+  {
+    district_en: "Mahottari",
+    district_np: "महोत्तरी",
+    zone: "Janakpur",
+    lat: 26.9,
+    long: 85.75
+  },
+  {
+    district_en: "Sarlahi",
+    district_np: "सर्लाही",
+    zone: "Janakpur",
+    lat: 26.9,
+    long: 85.55
+  },
+  {
+    district_en: "Sindhupalchok",
+    district_np: "सिन्धुपाल्चोक",
+    zone: "Bagmati",
+    lat: 27.95,
+    long: 85.7
+  },
+  {
+    district_en: "Rasuwa",
+    district_np: "रसुवा",
+    zone: "Bagmati",
+    lat: 28.15,
+    long: 85.2
+  },
+  {
+    district_en: "Nuwakot",
+    district_np: "नुवाकोट",
+    zone: "Bagmati",
+    lat: 27.95,
+    long: 85.15
+  },
+  {
+    district_en: "Dhading",
+    district_np: "धादिङ",
+    zone: "Bagmati",
+    lat: 27.95,
+    long: 84.8
+  },
+  {
+    district_en: "Kathmandu",
+    district_np: "काठमाडौँ",
+    zone: "Bagmati",
+    lat: 27.71,
+    long: 85.32
+  },
+  {
+    district_en: "Bhaktapur",
+    district_np: "भक्तपुर",
+    zone: "Bagmati",
+    lat: 27.67,
+    long: 85.42
+  },
+  {
+    district_en: "Lalitpur",
+    district_np: "ललितपुर",
+    zone: "Bagmati",
+    lat: 27.65,
+    long: 85.32
+  },
+  {
+    district_en: "Kavrepalanchok",
+    district_np: "काभ्रेपलान्चोक",
+    zone: "Bagmati",
+    lat: 27.58,
+    long: 85.53
+  },
+  {
+    district_en: "Makwanpur",
+    district_np: "मकवानपुर",
+    zone: "Narayani",
+    lat: 27.4,
+    long: 85.05
+  },
+  {
+    district_en: "Chitwan",
+    district_np: "चितवन",
+    zone: "Narayani",
+    lat: 27.5,
+    long: 84.5
+  },
+  {
+    district_en: "Bara",
+    district_np: "बारा",
+    zone: "Narayani",
+    lat: 27.1,
+    long: 84.85
+  },
+  {
+    district_en: "Parsa",
+    district_np: "पर्सा",
+    zone: "Narayani",
+    lat: 27.25,
+    long: 84.8
+  },
+  {
+    district_en: "Rautahat",
+    district_np: "रौतहट",
+    zone: "Narayani",
+    lat: 27.1,
+    long: 85.4
+  },
+  {
+    district_en: "Gorkha",
+    district_np: "गोरखा",
+    zone: "Gandaki",
+    lat: 28.25,
+    long: 84.6
+  },
+  {
+    district_en: "Kaski",
+    district_np: "कास्की",
+    zone: "Gandaki",
+    lat: 28.3,
+    long: 84.0
+  },
+  {
+    district_en: "Lamjung",
+    district_np: "लमजुङ",
+    zone: "Gandaki",
+    lat: 28.2,
+    long: 84.3
+  },
+  {
+    district_en: "Manang",
+    district_np: "मनाङ",
+    zone: "Gandaki",
+    lat: 28.6,
+    long: 84.1
+  },
+  {
+    district_en: "Syangja",
+    district_np: "स्याङ्जा",
+    zone: "Gandaki",
+    lat: 28.1,
+    long: 83.8
+  },
+  {
+    district_en: "Tanahun",
+    district_np: "तनहुँ",
+    zone: "Gandaki",
+    lat: 27.95,
+    long: 84.25
+  },
+  {
+    district_en: "Mustang",
+    district_np: "मुस्ताङ",
+    zone: "Dhaulagiri",
+    lat: 28.8,
+    long: 83.95
+  },
+  {
+    district_en: "Myagdi",
+    district_np: "म्याग्दी",
+    zone: "Dhaulagiri",
+    lat: 28.45,
+    long: 83.5
+  },
+  {
+    district_en: "Baglung",
+    district_np: "बागलुङ",
+    zone: "Dhaulagiri",
+    lat: 28.3,
+    long: 83.35
+  },
+  {
+    district_en: "Parbat",
+    district_np: "पर्वत",
+    zone: "Dhaulagiri",
+    lat: 28.23,
+    long: 83.7
+  },
+  {
+    district_en: "Arghakhanchi",
+    district_np: "अर्घाखाँची",
+    zone: "Lumbini",
+    lat: 27.95,
+    long: 83.15
+  },
+  {
+    district_en: "Gulmi",
+    district_np: "गुल्मी",
+    zone: "Lumbini",
+    lat: 28.05,
+    long: 83.25
+  },
+  {
+    district_en: "Kapilvastu",
+    district_np: "कपिलवस्तु",
+    zone: "Lumbini",
+    lat: 27.65,
+    long: 83.05
+  },
+  {
+    district_en: "Nawalparasi (West)",
+    district_np: "नवलपरासी (ब.सु.पु.)",
+    zone: "Lumbini",
+    lat: 27.55,
+    long: 83.85
+  },
+  {
+    district_en: "Nawalparasi (East)",
+    district_np: "नवलपुर",
+    zone: "Narayani/Gandaki",
+    lat: 27.65,
+    long: 84.1
+  },
+  {
+    district_en: "Palpa",
+    district_np: "पाल्पा",
+    zone: "Lumbini",
+    lat: 27.85,
+    long: 83.6
+  },
+  {
+    district_en: "Rupandehi",
+    district_np: "रुपन्देही",
+    zone: "Lumbini",
+    lat: 27.6,
+    long: 83.5
+  },
+  {
+    district_en: "Dang",
+    district_np: "दाङ",
+    zone: "Rapti",
+    lat: 28.05,
+    long: 82.35
+  },
+  {
+    district_en: "Pyuthan",
+    district_np: "प्युठान",
+    zone: "Rapti",
+    lat: 28.15,
+    long: 82.8
+  },
+  {
+    district_en: "Rolpa",
+    district_np: "रोल्पा",
+    zone: "Rapti",
+    lat: 28.4,
+    long: 82.55
+  },
+  {
+    district_en: "Rukum (West)",
+    district_np: "रूकुम पश्चिम",
+    zone: "Rapti",
+    lat: 28.65,
+    long: 82.45
+  },
+  {
+    district_en: "Rukum (East)",
+    district_np: "रूकुम पूर्व",
+    zone: "Rapti/Dhaulagiri",
+    lat: 28.6,
+    long: 83.0
+  },
+  {
+    district_en: "Salyan",
+    district_np: "सल्यान",
+    zone: "Rapti",
+    lat: 28.3,
+    long: 82.2
+  },
+  {
+    district_en: "Dolpa",
+    district_np: "डोल्पा",
+    zone: "Karnali",
+    lat: 29.05,
+    long: 82.95
+  },
+  {
+    district_en: "Humla",
+    district_np: "हुम्ला",
+    zone: "Karnali",
+    lat: 29.8,
+    long: 81.85
+  },
+  {
+    district_en: "Jumla",
+    district_np: "जुम्ला",
+    zone: "Karnali",
+    lat: 29.25,
+    long: 82.1
+  },
+  {
+    district_en: "Kalikot",
+    district_np: "कालिकोट",
+    zone: "Karnali",
+    lat: 29.25,
+    long: 81.65
+  },
+  {
+    district_en: "Mugu",
+    district_np: "मुगु",
+    zone: "Karnali",
+    lat: 29.55,
+    long: 82.4
+  },
+  {
+    district_en: "Banke",
+    district_np: "बाँके",
+    zone: "Bheri",
+    lat: 28.15,
+    long: 81.7
+  },
+  {
+    district_en: "Bardiya",
+    district_np: "बर्दिया",
+    zone: "Bheri",
+    lat: 28.35,
+    long: 81.3
+  },
+  {
+    district_en: "Dailekh",
+    district_np: "दैलेख",
+    zone: "Bheri",
+    lat: 28.8,
+    long: 81.7
+  },
+  {
+    district_en: "Jajarkot",
+    district_np: "जाजरकोट",
+    zone: "Bheri",
+    lat: 28.75,
+    long: 82.15
+  },
+  {
+    district_en: "Surkhet",
+    district_np: "सुर्खेत",
+    zone: "Bheri",
+    lat: 28.5,
+    long: 81.8
+  },
+  {
+    district_en: "Achham",
+    district_np: "अछाम",
+    zone: "Seti",
+    lat: 29.1,
+    long: 81.2
+  },
+  {
+    district_en: "Bajhang",
+    district_np: "बझाङ",
+    zone: "Seti",
+    lat: 29.55,
+    long: 81.2
+  },
+  {
+    district_en: "Bajura",
+    district_np: "बाजुरा",
+    zone: "Seti",
+    lat: 29.45,
+    long: 81.5
+  },
+  {
+    district_en: "Doti",
+    district_np: "डोटी",
+    zone: "Seti",
+    lat: 29.25,
+    long: 80.95
+  },
+  {
+    district_en: "Kailali",
+    district_np: "कैलाली",
+    zone: "Seti",
+    lat: 28.85,
+    long: 80.95
+  },
+  {
+    district_en: "Baitadi",
+    district_np: "बैतडी",
+    zone: "Mahakali",
+    lat: 29.5,
+    long: 80.6
+  },
+  {
+    district_en: "Dadeldhura",
+    district_np: "डडेलधुरा",
+    zone: "Mahakali",
+    lat: 29.2,
+    long: 80.5
+  },
+  {
+    district_en: "Darchula",
+    district_np: "दार्चुला",
+    zone: "Mahakali",
+    lat: 29.8,
+    long: 80.75
+  },
+  {
+    district_en: "Kanchanpur",
+    district_np: "कञ्चनपुर",
+    zone: "Mahakali",
+    lat: 28.9,
+    long: 80.35
+  }
+];
